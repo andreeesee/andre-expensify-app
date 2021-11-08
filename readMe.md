d208c211cd95316a0b8fbb351f986b76908ba4a4
See <john.andre.b.see@accenture.com>
2021-11-08 10:50:10 +0800

    Setup Production Webpack Build

---
+++ readMe.md
@@ -0,0 +1,23 @@
+# Git Commands
+
+git init - create a new git repo
+git status - view changes to your project
+git add - add files to staging area
+git commit - create a new commit with files from staging area
+git log - view recent commits
+
+# SSH Agent
+start ssh-agent
+ssh-agent -s
+eval $(ssh-agent)
+ssh-add /c/Users/john.andre.b.see/.ssh/id_rsa
+cd OneDrive\ -\ Accenture/Training/Reac\ JS/react-course-projects/expensify-app/
+
+# Git Push
+git remote add origin git@github.com:andreeesee/andre-expensify-app.git
+git remote
+git remote -v
+git branch -M main
+git push -u origin main
+
+# WebPack
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,49 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devServer: {
-        static: path.join(__dirname, 'public'),
-        historyApiFallback: true
-    },
-    devtool: 'eval-cheap-module-source-map'
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production;'
+    const CSSExtract = new MiniCssExtractPlugin({filename: 'styles.css'});
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },{
+				test: /\.s?css$/,
+				// use: ['style-loader', 'css-loader', 'sass-loader']		// loaded from right to left. 'style-loader' handles in-line CSS loading
+				use: [		// MiniCssExtractPlugin.loader will create a separate .CSS file. Loaded from bottom to top.
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,		// Tell each loader to create source maps
+							url: false				// For the url('/images/bg.jpg') Error: Can't resolve '/images/bg.jpg' in 'c:\....'
+						}
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}
+				]
+			}]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devServer: {
+            static: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        },
+        devtool: isProduction ? 'source-map' : 'inline-source-map'
+    };
 };
